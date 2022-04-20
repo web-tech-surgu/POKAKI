7ff05778d411d0ee23879a1dcb90d7e27676652b
web-tech-surgu <95065223+web-tech-surgu@users.noreply.github.com>
2022-04-20 20:39:29 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Блочная вёрстка</title>
-<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-<div id="container">
-	<div id="header">
-	<h2></h2>
-	</div>
-		 
-	<div id="navigation">
-	<h2></h2>
-	</div>
-		 
-	<div id="sidebar">
-	<h2></h2>
-	</div>
-		 
-	<div id="content">
-	<h2></h2>
-	</div>
-		 
-	<div id="clear">
-		 
-	</div>
-							   
-	<div id="footer">
-	<h2></h2>
-	</div>
-</div>
-</body>
-</html>
+<?php
+	$x = 10;
+	$x += --$x;
+	echo $x;
+ ?>
